3dae41cdd6e46d56578bbb377a57d5020b466d38
mukku69 <programing0803@gmail.com>
2021-06-10 10:15:40 +0000

    投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -27,9 +28,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
   end
 
 private
