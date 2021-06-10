3efd946fba73f878be29e229dc83b7e1e84d30c5
mukku69 <programing0803@gmail.com>
2021-06-10 13:03:59 +0000

    ログアウト機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+	def create
+		@post = post.find(params[:post_id])
+		@post_comment = postComment.new(post_comment_params)
+		@post_comment.post_id = @post.id
+		@post_comment.user_id = current_user.id
+		if @post_comment.save
+  		redirect_to post_path(@post.id)
+		else
+		  render 'posts/show'
+		end
+	end
+
+	def destroy
+		@post = post.find(params[:post_id])
+  	post_comment = @post.post_comments.find(params[:id])
+		post_comment.destroy
+		redirect_to request.referer
+	end
+
+	private
+	def post_comment_params
+		params.require(:post_comment).permit(:comment)
+	end
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts
 	has_many :favorites, dependent: :destroy
-	has_many :book_comments, dependent: :destroy
+	has_many :comments, dependent: :destroy
 end
