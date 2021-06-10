b3a95fcd18d11444c5383e430d702597171c0b80
mukku69 <programing0803@gmail.com>
2021-06-10 09:19:25 +0000

    投稿詳細ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def new
     @post = Post.new
   end
 
+  def index
+    @posts = Post.all
+  end
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root :to => 'homes#top'
 
-  resources :posts, only: [:index, :new, :show, :edit, :create, :destroy, :update] do
+  resources :users, only: [:show, :edit, :update]
+
+  resources :posts, only: [:new, :index, :show, :edit, :create, :destroy, :update] do
     resource :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
