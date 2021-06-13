ad96665aed9803b4bd4be232f4f79e959b3d8fa0
mukku69 <programing0803@gmail.com>
2021-06-13 14:38:12 +0000

    投稿一覧修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-	has_many :comments, dependent: :destroy
-	has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-	validates :title, presence: true
-	validates :body, presence: true, length: {maximum: 200}
+  validates :title, presence: true
+  validates :body, presence: true, length: {maximum: 200}
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
