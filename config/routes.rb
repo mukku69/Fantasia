061131c894dfec1980d1769d8c5d9a8ff729cedc
mukku69 <programing0803@gmail.com>
2021-06-15 15:20:55 +0000

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   get '/quit_confirm' => 'users#quit_confirm'
 
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy] do
+   resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
 
   resources :posts, only: [:new, :index, :show, :edit, :create, :destroy, :update] do
     resources :comments, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_075039) do
+ActiveRecord::Schema.define(version: 2021_06_15_143402) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2021_06_10_075039) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
