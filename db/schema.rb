ac1482b2e0d4fb7993cfc55fa25f407034cd266f
mukku69 <programing0803@gmail.com>
2021-06-17 11:30:38 +0000

    ジャンル機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_06_17_105746) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
