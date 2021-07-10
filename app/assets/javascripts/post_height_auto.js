ad80ba544e66b6afb198bdfb97863733877c5f3a
mukku69 <programing0803@gmail.com>
2021-07-10 23:59:59 +0900

    投稿一覧（ゲスト）修正

--- app/assets/javascripts/post_height_auto.js
+++ app/assets/javascripts/post_height_auto.js
@@ -1,8 +1,12 @@
-$(function() {
-  var $textarea = $('#textarea');
-  var lineHeight = parseInt($textarea.css('lineHeight'));
-  $textarea.on('input', function(e) {
-    var lines = ($(this).val() + '\n').match(/\n/g).length;
-    $(this).height(lineHeight * lines);
+ /* global $*/
+
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    var $textarea = $('#textarea');
+    var lineHeight = parseInt($textarea.css('lineHeight'));
+    $textarea.on('input', function(e) {
+      var lines = ($(this).val() + '\n').match(/\n/g).length;
+      $(this).height(lineHeight * lines);
+    });
   });
 });
